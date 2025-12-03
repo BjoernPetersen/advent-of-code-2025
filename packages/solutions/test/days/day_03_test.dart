0594e0a1f24c10b458a8f6a26b73a36b7b66589e
Björn Petersen <git@bjoernpetersen.net>
2025-12-03 08:15:37 +0100

    feat: implement day 3

---
+++ packages/solutions/test/days/day_03_test.dart
@@ -0,0 +1,61 @@
+import 'package:aoc/day.dart';
+import 'package:test/test.dart';
+
+import '../input_helper.dart';
+
+void main() {
+  const visualization = StubVisualization();
+  final dayNum = 03;
+  final day = getDay(dayNum);
+
+  group('day $dayNum', () {
+    group('part 1', () {
+      final part = day.partOne as IntPart;
+
+      for (final (example, expectedResult) in [('instructions-1', 357)]) {
+        test('example $example passes', () {
+          final reader = getExampleReader(dayNum, example);
+          expect(
+            part.calculate(visualization, reader.readLines()),
+            completion(expectedResult),
+          );
+        });
+      }
+
+      test('input passes', () {
+        final reader = getInputReader(dayNum);
+        expect(
+          part.calculate(visualization, reader.readLines()),
+          completion(17092),
+        );
+      });
+    });
+    group('part 2', () {
+      late final IntPart part;
+
+      setUpAll(() {
+        part = day.partTwo as IntPart;
+      });
+
+      for (final (example, expectedResult) in [
+        ('instructions-1', 3121910778619),
+      ]) {
+        test('example $example passes', () {
+          final reader = getExampleReader(dayNum, example);
+          expect(
+            part.calculate(visualization, reader.readLines()),
+            completion(expectedResult),
+          );
+        });
+      }
+
+      test('input passes', () {
+        final reader = getInputReader(dayNum);
+        expect(
+          part.calculate(visualization, reader.readLines()),
+          completion(170147128753455),
+        );
+      });
+    }, skip: day.partTwo == null);
+  });
+}
