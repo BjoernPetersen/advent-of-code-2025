7270e1eef47620f73940917d5affdcfe22cce430
Björn Petersen <git@bjoernpetersen.net>
2025-12-04 08:01:03 +0100

    Make web visualization barely usable

--- packages/frontend/lib/visualization/view.dart
+++ packages/frontend/lib/visualization/view.dart
@@ -29,7 +29,13 @@ final class VisualizationView extends StatelessWidget {
 
         final gridState = state.gridState;
         if (gridState != null) {
-          children.add(GridStateView(gridState.state, gridState.itemToString));
+          children.add(
+            GridStateView(
+              gridState.state,
+              gridState.itemToString,
+              showGridLines: false,
+            ),
+          );
         }
 
         return Column(mainAxisSize: .min, children: children);
@@ -66,19 +72,27 @@ final class ProgressView extends StatelessWidget {
 
 @immutable
 final class GridStateView<T> extends StatelessWidget {
+  final bool showGridLines;
   final Grid<T> grid;
   final String Function(T)? itemToString;
 
-  const GridStateView(this.grid, this.itemToString, {super.key});
+  const GridStateView(
+    this.grid,
+    this.itemToString, {
+    super.key,
+    required this.showGridLines,
+  });
 
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(color: Colors.black),
-          left: BorderSide(color: Colors.black),
-        ),
+        border: showGridLines
+            ? Border(
+                top: BorderSide(color: Colors.black),
+                left: BorderSide(color: Colors.black),
+              )
+            : null,
       ),
       child: Column(
         mainAxisSize: .min,
@@ -86,6 +100,7 @@ final class GridStateView<T> extends StatelessWidget {
           for (final row in grid.rows)
             GridStateRow(
               row: row,
+              showGridLines: showGridLines,
               itemToString: itemToString ?? (it) => it?.toString() ?? '',
             ),
         ],
@@ -95,11 +110,13 @@ final class GridStateView<T> extends StatelessWidget {
 }
 
 final class GridStateRow<T> extends StatelessWidget {
+  final bool showGridLines;
   final List<T> row;
   final String Function(T) itemToString;
 
   const GridStateRow({
     super.key,
+    required this.showGridLines,
     required this.row,
     required this.itemToString,
   });
@@ -111,16 +128,18 @@ final class GridStateRow<T> extends StatelessWidget {
       children: [
         for (final item in row)
           Container(
-            width: 20.0,
-            height: 20.0,
+            width: 6.0,
+            height: 6.0,
             decoration: BoxDecoration(
-              border: Border(
-                right: BorderSide(color: Colors.black),
-                bottom: BorderSide(color: Colors.black),
-              ),
+              border: showGridLines
+                  ? Border(
+                      right: BorderSide(color: Colors.black),
+                      bottom: BorderSide(color: Colors.black),
+                    )
+                  : null,
             ),
             alignment: .center,
-            child: Text(itemToString(item), style: TextStyle(fontSize: 15)),
+            child: Text(itemToString(item), style: TextStyle(fontSize: 5)),
           ),
       ],
     );
