5f6f59fb9e801fb9b6740f9367702c9683ebc9b1
Björn Petersen <git@bjoernpetersen.net>
2025-12-06 09:33:12 +0100

    feat: solve day 6 part 1

---
+++ packages/solutions/test/days/day_06_test.dart
@@ -0,0 +1,59 @@
+import 'package:aoc/day.dart';
+import 'package:test/test.dart';
+
+import '../input_helper.dart';
+
+void main() {
+  const visualization = StubVisualization();
+  final dayNum = 06;
+  final day = getDay(dayNum);
+
+  group('day $dayNum', () {
+    group('part 1', () {
+      final part = day.partOne as IntPart;
+
+      for (final (example, expectedResult) in [('instructions-1', 4277556)]) {
+        test('example $example passes', () {
+          final reader = getExampleReader(dayNum, example);
+          expect(
+            part.calculate(visualization, reader.readLines()),
+            completion(expectedResult),
+          );
+        });
+      }
+
+      test('input passes', () {
+        final reader = getInputReader(dayNum);
+        expect(
+          part.calculate(visualization, reader.readLines()),
+          completion(3968933219902),
+        );
+      });
+    });
+    group('part 2', () {
+      late final IntPart part;
+
+      setUpAll(() {
+        part = day.partTwo as IntPart;
+      });
+
+      for (final (example, expectedResult) in [('instructions-1', 0)]) {
+        test('example $example passes', () {
+          final reader = getExampleReader(dayNum, example);
+          expect(
+            part.calculate(visualization, reader.readLines()),
+            completion(expectedResult),
+          );
+        });
+      }
+
+      test('input passes', () {
+        final reader = getInputReader(dayNum);
+        expect(
+          part.calculate(visualization, reader.readLines()),
+          completion(0),
+        );
+      });
+    }, skip: day.partTwo == null);
+  });
+}
