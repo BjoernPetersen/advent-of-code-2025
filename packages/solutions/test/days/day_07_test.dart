09c64248229b6eac71e1d6b3ee759fb7d1e930f8
Björn Petersen <git@bjoernpetersen.net>
2025-12-07 09:36:03 +0100

    feat: solve day 7 part 2

--- packages/solutions/test/days/day_07_test.dart
+++ packages/solutions/test/days/day_07_test.dart
@@ -37,7 +37,7 @@ void main() {
         part = day.partTwo as IntPart;
       });
 
-      for (final (example, expectedResult) in [('instructions-1', 0)]) {
+      for (final (example, expectedResult) in [('instructions-1', 40)]) {
         test('example $example passes', () {
           final reader = getExampleReader(dayNum, example);
           expect(
@@ -51,7 +51,7 @@ void main() {
         final reader = getInputReader(dayNum);
         expect(
           part.calculate(visualization, reader.readLines()),
-          completion(0),
+          completion(231507396180012),
         );
       });
     }, skip: day.partTwo == null);
